6b15096e65e2980d1e9266a1c5356cd839775ba7
MrYuriyS <yura_salimov@meta.ua>
2017-01-07 12:21:34 +0200

    User roles moved to the file from User.class.

---
+++ src/main/java/com/salimov/yurii/enums/UserRole.java
@@ -0,0 +1,47 @@
+package com.salimov.yurii.enums;
+
+import java.util.Arrays;
+
+/**
+ * The role of some user.
+ *
+ * @author Yuriy Salimov (yuriy.alex.salimov@gmail.com)
+ * @version 1.0
+ * @see com.salimov.yurii.entity.User
+ */
+public enum UserRole {
+
+    /**
+     * The superman role.
+     */
+    SUPERMAN,
+
+    /**
+     * The superman role.
+     */
+    ADMIN,
+
+    /**
+     * The superman role.
+     */
+    CLIENT,
+
+    /**
+     * The another role.
+     */
+    ANOTHER;
+
+    /**
+     * Returns the elements of this enum class or null if this
+     * Class object does not represent an enum type.
+     *
+     * @return an array containing the values comprising the enum class
+     * represented by this Class object in the order they're
+     * declared, or null if this Class object does not
+     * represent an enum type
+     */
+    public static UserRole[] getEnumConstants() {
+        UserRole[] roles = UserRole.class.getEnumConstants();
+        return Arrays.copyOfRange(roles, 1, roles.length);
+    }
+}
