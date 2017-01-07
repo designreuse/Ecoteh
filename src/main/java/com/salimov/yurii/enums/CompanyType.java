741fa553fd8401252655844f9b8360a91504c150
MrYuriyS <yura_salimov@meta.ua>
2017-01-07 12:21:59 +0200

    Company types moved to the file from Company.class.

---
+++ src/main/java/com/salimov/yurii/enums/CompanyType.java
@@ -0,0 +1,21 @@
+package com.salimov.yurii.enums;
+
+/**
+ * The types of some company.
+ *
+ * @author Yuriy Salimov (yuriy.alex.salimov@gmail.com)
+ * @version 1.0
+ * @see com.salimov.yurii.entity.Company
+ */
+public enum CompanyType {
+
+    /**
+     * The main company type.
+     */
+    MAIN,
+
+    /**
+     * The partner company type.
+     */
+    PARTNER
+}
