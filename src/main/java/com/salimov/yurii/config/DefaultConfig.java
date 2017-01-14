969cb9c9d35c15dddc47411c47ff540d483f0bdf
MrYuriyS <yura_salimov@meta.ua>
2017-01-14 14:59:21 +0200

    Delivers the getting a authenticated user in a single method. Added a missing documentation.

--- src/main/java/com/salimov/yurii/config/DefaultConfig.java
+++ src/main/java/com/salimov/yurii/config/DefaultConfig.java
@@ -3,25 +3,39 @@ package com.salimov.yurii.config;
 import com.salimov.yurii.entity.User;
 import com.salimov.yurii.enums.UserRole;
 import com.salimov.yurii.util.translator.Translator;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Class with default configuration.
+ * Class to work with the default users.
  *
  * @author Yurii Salimov (yurii.alex.salimov@gmail.com)
  * @version 1.0
+ * @see User
  */
 public final class DefaultConfig {
 
+    /**
+     * It is admin requests is enabled.
+     */
     private static boolean adminEnabled;
+
+    /**
+     * It is client requests is enabled.
+     */
     private static boolean clientEnabled;
 
+    /**
+     * The map of a default users.
+     */
     private final static Map<String, User> USERS;
 
+    /**
+     * Static block for initialization of map
+     * with a default users.
+     */
     static {
         adminEnabled = true;
         clientEnabled = true;
@@ -30,26 +44,78 @@ public final class DefaultConfig {
         addSuperAdmin();
     }
 
+    /**
+     * Returns a default user with the username.
+     *
+     * @param username a name of user to return.
+     * @return The user if it exist,
+     * {@code null} otherwise.
+     */
     public static User getDefaultUser(final String username) {
         return USERS.get(username);
     }
 
+    /**
+     * Returns a default admin.
+     *
+     * @return The default admin.
+     */
     public static User getDefaultAdmin() {
         return getDefaultUser("admin");
     }
 
+    /**
+     * Returns a default super admin.
+     *
+     * @return The default super admin.
+     */
     public static User getSuperAdmin() {
         return getDefaultUser("super");
     }
 
+    /**
+     * Returns {@code true} if admin requests is enabled,
+     * {@code false} otherwise.
+     *
+     * @return {@code true} if admin requests is enabled,
+     * {@code false} otherwise.
+     */
     public static boolean isAdminEnabled() {
         return adminEnabled;
     }
 
+    /**
+     * Returns {@code true} if client requests is enabled,
+     * {@code false} otherwise.
+     *
+     * @return {@code true} if client requests is enabled,
+     * {@code false} otherwise.
+     */
     public static boolean isClientEnabled() {
         return clientEnabled;
     }
 
+    /**
+     * Enables requests for clients.
+     */
+    public static void on() {
+        if (checkUser() || checkSuperUser()) {
+            clientEnabled = true;
+        }
+    }
+
+    /**
+     * Disables requests for clients.
+     */
+    public static void off() {
+        if (checkUser() || checkSuperUser()) {
+            clientEnabled = false;
+        }
+    }
+
+    /**
+     * Enables requests for clients and admins.
+     */
     public static void superOn() {
         if (checkSuperUser()) {
             adminEnabled = true;
@@ -57,12 +123,9 @@ public final class DefaultConfig {
         }
     }
 
-    public static void on() {
-        if (checkUser() || checkSuperUser()) {
-            clientEnabled = true;
-        }
-    }
-
+    /**
+     * Disables requests for clients and admins.
+     */
     public static void superOff() {
         if (checkSuperUser()) {
             adminEnabled = false;
@@ -70,37 +133,56 @@ public final class DefaultConfig {
         }
     }
 
-    public static void off() {
-        if (checkUser() || checkSuperUser()) {
-            clientEnabled = false;
-        }
-    }
-
-    private static boolean checkSuperUser() {
-        return check(UserRole.SUPERMAN);
-    }
-
+    /**
+     * Checks a role of an authorized user.
+     * Authorized user must have the ADMIN role.
+     *
+     * @return {@code true} if an authorized user has
+     * the ADMIN role, {@code false} otherwise.
+     */
     private static boolean checkUser() {
         return check(UserRole.ADMIN);
     }
 
-    private static boolean check(final UserRole role) {
-        boolean result = false;
-        final Authentication authentication =
-                SecurityContextHolder.getContext()
-                        .getAuthentication();
-        if (authentication != null) {
-            final User user = (User) authentication.getPrincipal();
-            result = (
-                    user != null
-            ) && (
-                    user.getRole()
-                            .equals(role)
-            );
-        }
-        return result;
+    /**
+     * Checks a role of an authorized user.
+     * Authorized user must have the SUPERMAN role.
+     *
+     * @return {@code true} if an authorized user has
+     * the SUPERMAN role, {@code false} otherwise.
+     */
+    private static boolean checkSuperUser() {
+        return check(UserRole.SUPERMAN);
     }
 
+    /**
+     * Checks a role of an authorized user.
+     *
+     * @param role a role to check.
+     * @return {@code true} if an authorized user has the role,
+     * {@code false} otherwise.
+     */
+    private static boolean check(final UserRole role) {
+        final User user = getAuthenticatedUser();
+        return user != null &&
+                user.getRole()
+                        .equals(role);
+    }
+
+    /**
+     * Returns authenticated user.
+     *
+     * @return The authenticated user.
+     * @see User
+     */
+    private static User getAuthenticatedUser() {
+        return (User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+    }
+
+    /**
+     * Adds a default admin to global map.
+     */
     private static void addDefaultAdmin() {
         final User user = createUser(
                 "Default Admin",
@@ -114,6 +196,9 @@ public final class DefaultConfig {
         );
     }
 
+    /**
+     * Adds a super admin to global map.
+     */
     private static void addSuperAdmin() {
         final User user = createUser(
                 "Super Admin",
@@ -127,6 +212,16 @@ public final class DefaultConfig {
         );
     }
 
+    /**
+     * Creates and returns a new user.
+     *
+     * @param name     a name of the new user.
+     * @param login    a login of the new user.
+     * @param password a password of the new user.
+     * @param role     a role of the new user.
+     * @return The new user.
+     * @see User
+     */
     private static User createUser(
             final String name,
             final String login,
