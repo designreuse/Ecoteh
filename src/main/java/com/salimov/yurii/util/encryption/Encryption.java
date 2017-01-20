0bc586fe86eb53252f9781573adb5710121ccc79
MrYuriyS <yura_salimov@meta.ua>
2017-01-20 14:24:01 +0200

    Added a missing documentation.

--- src/main/java/com/salimov/yurii/util/encryption/Encryption.java
+++ src/main/java/com/salimov/yurii/util/encryption/Encryption.java
@@ -3,27 +3,32 @@ package com.salimov.yurii.util.encryption;
 import com.salimov.yurii.util.translator.AsciiImpl;
 
 /**
+ * The class implements methods for data encryption.
+ *
  * @author Yuriy Salimov (yuriy.alex.salimov@gmail.com)
  * @version 1.0
+ * @see IEncryption
  */
 public class Encryption implements IEncryption {
 
     /**
-     *
+     * The value to encrypt or to decrypt.
      */
     private final String value;
 
     /**
+     * Constructor.
      *
-     * @param value
+     * @param value a value to encrypt or to decrypt.
      */
     public Encryption(final String value) {
         this.value = value;
     }
 
     /**
+     * Encrypts a data.
      *
-     * @return
+     * @return The encrypted data.
      */
     @Override
     public String encrypt() {
@@ -31,14 +36,20 @@ public class Encryption implements IEncryption {
     }
 
     /**
+     * Decrypts a date.
      *
-     * @return
+     * @return The decrypted data
      */
     @Override
     public String decrypt() {
         return new AsciiImpl(this.value).from();
     }
 
+    /**
+     * Returns a value to encrypt or to decrypt.
+     *
+     * @return The value to encrypt or to decrypt.
+     */
     public String getValue() {
         return this.value;
     }
--- src/main/java/com/salimov/yurii/util/encryption/IEncryption.java
+++ src/main/java/com/salimov/yurii/util/encryption/IEncryption.java
@@ -1,20 +1,25 @@
 package com.salimov.yurii.util.encryption;
 
 /**
+ * The interface describes the methods for data encryption.
+ *
  * @author Yuriy Salimov (yuriy.alex.salimov@gmail.com)
  * @version 1.0
+ * @see Encryption
  */
 public interface IEncryption {
 
     /**
+     * Encrypts a data.
      *
-     * @return
+     * @return The encrypted data.
      */
     String encrypt();
 
     /**
+     * Decrypts a date.
      *
-     * @return
+     * @return The decrypted data
      */
     String decrypt();
 }
