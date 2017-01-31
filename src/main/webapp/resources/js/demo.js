1997a1870b3fa84f8bf8b0fcd3514d22e818a473
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2017-01-31 17:34:13 +0200

    Added modal window script.

---
+++ src/main/webapp/resources/js/demo.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.open').click(function () {
+        type = $(this).attr('data-type');
+        $('.overlay-container').fadeIn(function () {
+            window.setTimeout(function () {
+                $('.window-container.' + type).addClass('window-container-visible');
+            }, 100);
+        });
+    });
+    $('.close').click(function () {
+        $('.overlay-container').fadeOut().end().find('.window-container').removeClass('window-container-visible');
+    });
+});
