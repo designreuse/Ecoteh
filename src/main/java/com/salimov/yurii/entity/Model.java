b2486124f2f26f2c5448f835a1f4565219d31dc3
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2017-02-13 13:03:58 +0200

    Removed deprecated methods.

--- src/main/java/com/salimov/yurii/entity/Model.java
+++ src/main/java/com/salimov/yurii/entity/Model.java
@@ -1,14 +1,10 @@
 package com.salimov.yurii.entity;
 
 import com.salimov.yurii.entity.interfaces.IModel;
+import org.apache.log4j.Logger;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Random;
-import java.util.TimeZone;
 
 /**
  * The abstract superclass class implements a set of standard methods
@@ -31,34 +27,17 @@ public abstract class Model<E extends Number>
         implements IModel<E>, Serializable, Cloneable {
 
     /**
-     * Default length of new generating string,
-     */
-    public static final int CODE_LENGTH = 6;
-
-    /**
-     * Default pattern to generated new string.
-     */
-    public static final char[] CODE_PATTERN =
-            "ECOTEHANDTERMODRUCK1234567890".toCharArray();
-
-    /**
-     * It is s an object for date/time formatting subclasses which formats
-     * and parses dates or time in a language-independent manner.
-     */
-    public static final String DATE_PATTERN = "d MMM yyyy, HH:mm";
-
-    /**
-     * Represents a timezone offset, and also figures out daylight savings.
-     */
-    public static final String TIME_ZONE = "GMT+3";
-
-    /**
      * It is used during deserialization to verify that
      * the sender and receiver of a serialized object have
      * loaded classes for that object that are compatible
      * with respect to serialization.
      */
     private static final long serialVersionUID = 1L;
+
+    /**
+     * The object for logging information.
+     */
+    private static final Logger LOGGER = Logger.getLogger(Model.class);
 
     /**
      * The unique identifier for each object.
@@ -105,7 +84,7 @@ public abstract class Model<E extends Number>
         try {
             model = (Model) super.clone();
         } catch (CloneNotSupportedException ex) {
-            ex.printStackTrace();
+            LOGGER.error(ex.getMessage(), ex);
         }
         return model;
     }
@@ -180,62 +159,6 @@ public abstract class Model<E extends Number>
     }
 
     /**
-     * Creates a random string of predetermined pattern and length.
-     *
-     * @param pattern a pattern of the random string.
-     * @param length  a length of the random string.
-     * @return The random string.
-     */
-    public static String createRandomString(
-            final char[] pattern,
-            final int length
-    ) {
-        final StringBuilder sb = new StringBuilder();
-        if ((pattern != null)
-                && (pattern.length != 0)
-                && (length > 0)) {
-            final Random random = new Random(System.nanoTime());
-            for (int i = 0; i < length; i++) {
-                sb.append(
-                        pattern[random.nextInt(pattern.length)]
-                );
-            }
-        }
-        return sb.toString();
-    }
-
-    /**
-     * Creates a random number of predetermined length.
-     *
-     * @param length a length of the random number.
-     * @return The random number.
-     */
-    public static int createRandomNumber(int length) {
-        int number = 0;
-        if (length > 0) {
-            final Random random = new Random(System.nanoTime());
-            for (int i = 0; i < length; i++) {
-                number += (random.nextInt(8) + 1) * (int) Math.pow(10, i);
-            }
-        }
-        return number;
-    }
-
-    /**
-     * Returns a model date in string format.
-     *
-     * @param date a date to translate in string.
-     * @return The model string-date.
-     */
-    public static String getDateToString(final Date date) {
-        return getDateToStringWithFormat(
-                date,
-                new SimpleDateFormat(DATE_PATTERN),
-                TimeZone.getTimeZone(TIME_ZONE)
-        );
-    }
-
-    /**
      * Returns a hash code value for the object.
      * This method is supported for the benefit
      * of hash tables such as those provided by HashMap.
@@ -244,26 +167,4 @@ public abstract class Model<E extends Number>
      */
     @Override
     public abstract int hashCode();
-
-    /**
-     * Returns a model date in string format with some date format.
-     *
-     * @param date       a date to translate in string.
-     * @param dateFormat is an object for date/time formatting subclasses
-     *                   which formats and parses dates or time in
-     *                   a language-independent manner.
-     * @param timeZone   represents a timezone offset,
-     *                   and also figures out daylight savings.
-     * @return The model string-date.
-     */
-    private static String getDateToStringWithFormat(
-            final Date date,
-            final DateFormat dateFormat,
-            final TimeZone timeZone
-    ) {
-        dateFormat.setTimeZone(timeZone);
-        return dateFormat.format(
-                date != null ? date : new Date()
-        );
-    }
 }
