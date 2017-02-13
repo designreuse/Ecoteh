9e089145fd1610197ba8665c2473617f31a1257f
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2017-02-13 13:01:21 +0200

    Added a new class which implements a set of methods for compressing data.

---
+++ src/main/java/com/salimov/yurii/util/compressor/HtmlPress.java
@@ -0,0 +1,47 @@
+package com.salimov.yurii.util.compressor;
+
+import com.googlecode.htmlcompressor.compressor.Compressor;
+import com.googlecode.htmlcompressor.compressor.HtmlCompressor;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+/**
+ * The class implements a set of methods for compressing data.
+ *
+ * @author Yuriy Salimov (yuriy.alex.salimov@gmail.com)
+ * @version 1.0
+ */
+public final class HtmlPress implements Compressor {
+
+    /**
+     * Instance describes compressor methods.
+     */
+    private static Compressor compressor;
+
+    /**
+     * Compresses the given source and returns a compressed result.
+     *
+     * @param source The source to compress.
+     * @return Compressed result.
+     */
+    @Override
+    public String compress(final String source) {
+        String result = null;
+        if (isNotBlank(source)) {
+            if (compressor == null) {
+                initCompress();
+            }
+            result = compressor.compress(source);
+        }
+        return result;
+    }
+
+    /**
+     * Initializes the Compressor object.
+     */
+    private static void initCompress() {
+        final HtmlCompressor compressor = new HtmlCompressor();
+        compressor.setRemoveIntertagSpaces(true);
+        HtmlPress.compressor = compressor;
+    }
+}
