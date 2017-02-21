48b88719008475817bf3776f7c5200af69640eb5
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2017-02-21 16:32:57 +0200

    Added a new repository for working with messages.

---
+++ src/main/java/com/salimov/yurii/repository/MessageRepository.java
@@ -0,0 +1,16 @@
+package com.salimov.yurii.repository;
+
+import com.salimov.yurii.entity.Message;
+import com.salimov.yurii.entity.Response;
+
+/**
+ * The interface provides a set of JPA methods
+ * for working {@link Message} objects with a database.
+ *
+ * @author Yurii Salimov (yuriy.alex.salimov@gmail.com)
+ * @version 1.0
+ * @see DataRepository
+ * @see Response
+ */
+public interface MessageRepository extends DataRepository<Message> {
+}
