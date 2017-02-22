c9569feec4d8a71f3f71572ef9a6de3fdb74b1d9
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2017-02-22 12:20:03 +0200

    Added the types of files.

---
+++ src/main/java/com/salimov/yurii/enums/FileType.java
@@ -0,0 +1,26 @@
+package com.salimov.yurii.enums;
+
+/**
+ * The types of files.
+ *
+ * @author Yuriy Salimov (yuriy.alex.salimov@gmail.com)
+ * @version 1.0
+ * @see com.salimov.yurii.entity.File
+ */
+public enum FileType {
+
+    /**
+     * Type of static files.
+     */
+    STATIC,
+
+    /**
+     * Type of slide files.
+     */
+    SLIDE,
+
+    /**
+     * Type of other files.
+     */
+    OTHER
+}
