37f011784d814847bc234f9668aeac7443fceb59
YuriiSalimov <yuriy.alex.salimov@gmail.com>
2017-05-10 14:28:44 +0300

    Test code refactoring.

--- src/test/java/ua/com/ecoteh/controller/authorization/AuthorizationControllerTest.java
+++ src/test/java/ua/com/ecoteh/controller/authorization/AuthorizationControllerTest.java
@@ -24,7 +24,7 @@ public class AuthorizationControllerTest {
     public void whenLoginPageThenReturnSomeModelAndView() {
         checkModelAndView(
                 controller.loginPage(),
-                "login/login",
+                "redirect:/admin/menu",
                 null
         );
     }
