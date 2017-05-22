5acc0a57693a81a036613bc1232da6da95f02f02
YuriiSalimov <yuriy.alex.salimov@gmail.com>
2017-05-22 12:31:30 +0300

    Added a Favicon controller.

---
+++ src/main/java/ua/com/ecoteh/controller/client/FaviconController.java
@@ -0,0 +1,51 @@
+package ua.com.ecoteh.controller.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ua.com.ecoteh.entity.File;
+import ua.com.ecoteh.enums.FileType;
+import ua.com.ecoteh.service.data.FileService;
+
+/**
+ * Favicon controller.
+ *
+ * @author Yurii Salimov (yuriy.alex.salimov@gmail.com)
+ */
+@Controller
+public class FaviconController {
+
+    /**
+     * The implementation of the interface describes a set of methods
+     * for working with objects of the {@link File} class.
+     */
+    private final FileService fileService;
+
+    /**
+     * Constructor.
+     *
+     * @param fileService the implementation of the {@link File} interface.
+     */
+    @Autowired
+    public FaviconController(final FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    /**
+     * Returns a favicon.ico.
+     * Request mapping: /favicon.ico
+     * Method: GET
+     *
+     * @return The favicon.ico URL.
+     */
+    @RequestMapping(
+            value = "/favicon.ico",
+            method = RequestMethod.GET
+    )
+    public String getFavicon() {
+        System.out.println("!!!!! FAVICON !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        final File favicon = this.fileService.getLastByType(FileType.FAVICON);
+        return "forward:" + favicon.getUrl();
+    }
+}
